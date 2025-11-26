74e8dfe2c5cbe6f3ad8936a811ec8a915551bc44
huangkai <h1770360848@outlook.com>
2025-11-26 15:55:04 +0800

    feat(lab): finish lab02

--- lab/lab02/lab02.py
+++ lab/lab02/lab02.py
@@ -13,7 +13,7 @@ def composite_identity(f, g):
     >>> b1(4)                            # (4 + 1) ** 2 != 4 ** 2 + 1
     False
     """
-    "*** YOUR CODE HERE ***"
+    return lambda x: f(g(x)) == g(f(x))
 
 
 def sum_digits(y):
@@ -59,7 +59,9 @@ def count_cond(condition):
     >>> count_primes(20)   # 2, 3, 5, 7, 11, 13, 17, 19
     8
     """
-    "*** YOUR CODE HERE ***"
+    def count(n: int):
+        return sum(condition(n, i) for i in range(1, n+1))
+    return count
 
 
 def multiple(a, b):
